7a10c4586acbc592194b891f8a92dd9e969a6c82
David English <s175252@ed.ac.uk>
2020-12-04 02:01:20 +0000

    David added some challenge script guidance

---
+++ challenge/challenge_script.R
@@ -0,0 +1,36 @@
+# Template for Challenge Section of Tutorial
+# Script Created for the "Going on the Grid" Tutorial by David English 
+# https://eddatascienceees.github.io/tutorial-de356/
+
+# Load all the packages we'll be using for this tutorial
+# Library----
+library(tidyverse)
+library(imager)
+library(grid)
+library(gridExtra)
+library(gridtext)
+
+# Set Working Directory----
+setwd("Insert File Pathway to Repo Here")
+
+# Data Wrangling----
+iris <- iris
+
+# For those not working with RStudio
+# iris <- read.csv("data/iris.csv")
+
+# Once set, complete the challenge below! 
+# We've added some section headers to keep you on track for what needs to be done
+
+# Create the Table Grob----
+
+# Create the Scatterplot----
+
+# Import the Flower Image----
+
+# Create the Text Grob----
+
+# Arrange All of the Objects Together----
+
+# If you're stumped, go back to the end to the end of the tutorial to see the example
+# answer key for a push in the right direction. 
